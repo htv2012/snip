d43bd83d4cc0d65b913fd40a9ab37a90e73c6744
Hai Vu <haivu2004@gmail.com>
2024-09-03 21:05:57 -0700

    Add fzf to select file

--- snip/cli.py
+++ snip/cli.py
@@ -1,10 +1,36 @@
 import argparse
 import logging
 import logging.config
+import os
 import pathlib
+import shutil
+import subprocess
+import tempfile
 
 from . import snip
 from .config import DATA_DIR, load
+
+
+def select_file(root: pathlib.Path):
+    """Select a file from data dir using fzf."""
+    if not shutil.which("fzf"):
+        return None
+
+    here = os.getcwd()
+    os.chdir(root)
+
+    with tempfile.TemporaryFile(mode="w+", encoding="utf-8") as stdout:
+        subprocess.run(
+            ["fzf"],
+            text=True,
+            stdout=stdout,
+        )
+        stdout.seek(0)
+        selected = stdout.read().strip()
+        logging.debug("selected=%r", selected)
+
+    os.chdir(here)
+    return selected
 
 
 def main():
@@ -15,8 +41,8 @@ def main():
     put_parser.add_argument("filename")
 
     get_parser = sub_parser.add_parser("get")
-    get_parser.add_argument("filename")
-    get_parser.add_argument("vars", nargs="*")
+    get_parser.add_argument("-f", "--file")
+    get_parser.add_argument("-d", "--define", nargs="*", action="extend", default=[])
 
     options = parser.parse_args()
     logging.debug("options: %r", options)
@@ -27,10 +53,13 @@ def main():
     if options.action == "put":
         snip.put(options.filename, root)
     elif options.action == "get":
-        # TODO: make filename optional, use fzf to select
-        variables = dict(token.split("=") for token in options.vars)
+        if options.file is None:
+            options.file = select_file(root)
+        if options.file is None:
+            raise SystemExit("Please specify a file name via -f, or install fzf")
+        variables = dict(token.split("=") for token in options.define)
         logging.debug("variables=%r", variables)
-        snip.get(options.filename, root, variables)
+        snip.get(options.file, root, variables)
 
 
 if __name__ == "__main__":
