2a6a04d13df6308c57a7a26bb5b21763a7c1f1d8
Hai Vu <haivu2004@gmail.com>
2025-04-27 15:59:37 -0700

    Fix test that failed only on 3.9

--- src/snip/pathtools.py
+++ src/snip/pathtools.py
@@ -1,12 +1,13 @@
 import logging
 import logging.config
 import pathlib
+from typing import Union
 
 logging.config.fileConfig(pathlib.Path(__file__).with_name("logging.ini"))
 LOGGER = logging.getLogger("root.cli")
 
 
-def is_empty_dir(path: str | pathlib.Path) -> bool:
+def is_empty_dir(path: Union[str, pathlib.Path]) -> bool:
     """Return True if the directory is empty, False otherwise."""
     path = pathlib.Path(path)
     if not path.is_dir():
