788e7b565fbaaf3da4ad53bb29529e626fb7cd24
Hai Vu <haivu2004@gmail.com>
2025-04-27 14:50:56 -0700

    Add more tests against config.py

--- src/test/test_load_config.py
+++ src/test/test_load_config.py
@@ -1,7 +1,9 @@
 import json
 import pathlib
 
-from snip.config import load
+import pytest
+
+from snip.config import get_or_create_data_dir, load
 
 CONFIG_PATH = pathlib.Path("~/.config/snip.json").expanduser()
 CONFIG_FOUND = CONFIG_PATH.exists()
@@ -18,14 +20,34 @@ def teardown_module():
         CONFIG_PATH.write_text(BAK.read_text())
 
 
-def test_not_exist():
+def test_load_not_exist():
     CONFIG_PATH.unlink(missing_ok=True)
     assert load() == {"data-dir": str(pathlib.Path("~/.local/share/snip").expanduser())}
 
 
-def test_exist():
+def test_load_exist():
     config = {"data-dir": "/tmp"}
     with open(CONFIG_PATH, "w") as stream:
         json.dump(config, stream)
 
     assert load() == config
+
+
+@pytest.mark.parametrize("exists", [False, True])
+def test_get(tmp_path, exists: bool):
+    CONFIG_PATH.unlink(missing_ok=True)
+    data_dir = tmp_path / "data"
+    assert not data_dir.exists()
+    assert not data_dir.is_dir()
+
+    if exists:
+        data_dir.mkdir()
+
+    config = {"data-dir": str(data_dir)}
+    with open(CONFIG_PATH, "w") as stream:
+        json.dump(config, stream)
+
+    actual = get_or_create_data_dir()
+    assert actual == data_dir
+    assert actual.exists()
+    assert actual.is_dir()
