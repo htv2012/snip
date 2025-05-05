8ab99f757b22b581c18ce41263ce063c56c37fc7
Hai Vu <haivu2004@gmail.com>
2025-05-04 22:03:21 -0700

    Add more exploratory tests, will clean up later

--- src/test/test_copy_to_clipboard.py
+++ src/test/test_copy_to_clipboard.py
@@ -1,14 +1,34 @@
 """
 Test copy_to_clipboard() function
 """
+
 import shutil
-import pathlib
+from unittest import mock
 
 import pytest
 
 from snip.desktop import copy_to_clipboard
 
 SAMPLE_TEXT = "This is a test"
+
+
+@mock.patch("platform.system", lambda: "Darwin")
+@mock.patch("subprocess.run")
+def test_on_darwin(mock_run):
+    copy_to_clipboard(SAMPLE_TEXT)
+    call_args = mock_run.call_args
+    assert call_args.args == (["pbcopy"],)
+    assert call_args.kwargs["input"] == SAMPLE_TEXT
+
+
+@mock.patch("platform.system", lambda: "Linux")
+@mock.patch("subprocess.run")
+def test_on_darwin(mock_run):
+    copy_to_clipboard(SAMPLE_TEXT)
+    call_args = mock_run.call_args
+    assert call_args.args == (["xsel", "-s"],)
+    assert call_args.kwargs["input"] == SAMPLE_TEXT
+
 
 
 @pytest.fixture
@@ -55,14 +75,14 @@ def setup_command(request, monkeypatch, tmp_path):
     monkeypatch.setattr("platform.system", lambda: platform_name)
     monkeypatch.setenv("PATH", str(tmp_path))
 
+
 @pytest.mark.parametrize(
     "setup_command",
     [
         pytest.param(("Darwin", "pbcopy"), id="macos"),
         pytest.param(("Linux", "xsel"), id="linux"),
-    ]
+    ],
 )
 def test_found(capfd, setup_command):
     copy_to_clipboard(SAMPLE_TEXT)
     assert capfd.readouterr().out == SAMPLE_TEXT
-
